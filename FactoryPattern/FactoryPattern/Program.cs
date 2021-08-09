a90c537881fffcc413a99be07bb4b17dfc736bc8
Agustin Jose <agustinwawrzyk@protonmail.com>
2021-08-09 17:31:43 -0300

    Patron Factory

---
+++ FactoryPattern/FactoryPattern/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Sandwich> Sandwiches = new List<Sandwich>();
+            Sandwiches.Add(new MilanesaSandwich());
+            Sandwiches.Add(new JamonSandwich());
+
+            foreach(var sandwich in Sandwiches)
+            {
+                Console.WriteLine("\nSandwich : " + sandwich.GetType().Name + " ");
+                foreach(var ingrediente in sandwich.Ingredientes)
+                {
+                    Console.WriteLine("ingrediente: " + ingrediente.GetType().Name);
+                }
+            }
+            Console.ReadKey();
+        }   
+    }
+}
